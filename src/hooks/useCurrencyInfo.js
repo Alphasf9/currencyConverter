669d099230eaa1c2f0efd6e68d132e19b12198fe
Alphasf9 <134485025+Alphasf9@users.noreply.github.com>
2023-12-23 21:01:25 +0530

    Update useCurrencyInfo.js

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,17 +1,16 @@
-import { useEffect, useState } from "react";  // designing custom hooks
+import {useEffect, useState} from "react"
 
 
-function useCurrencyInfo(currency) {
+function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://community-neutrino-currency-conversion.p.rapidapi.com/convert`)
-            .then((res) => res.json())
-            .then((res) => setData(res[currency]))
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
         console.log(data);
     }, [currency])
     console.log(data);
-    return data;
+    return data
 }
-
 
 export default useCurrencyInfo;
